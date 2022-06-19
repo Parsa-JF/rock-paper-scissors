cc46f11226a77ef01362f8c8c9ab06699992b4ad
Parsa-JF <parsafares@gmail.com>
2022-06-18 21:52:45 -0700

    finalize UI

--- script.js
+++ script.js
@@ -42,11 +42,24 @@ const r = document.querySelector('#r');
 const p = document.querySelector('#p');
 const s = document.querySelector('#s');
 const ROUNDWINNER = document.querySelector('#roundWinner');
+const COMPUTERSELECTION = document.querySelector('#computerSelection');
+const modal = document.getElementById("myModal");
+const span = document.getElementById("close");
+const MODAL_TEXT = document.getElementById("MODAL_TEXT");
 
 
 function displayRoundWinner()
 {
     ROUNDWINNER.textContent = roundWinner; 
+    if(roundWinner == "Player Won!")
+    {
+        ROUNDWINNER.style.color = "green";
+    }
+    else if(roundWinner == "Draw!")
+    {
+        ROUNDWINNER.style.color = "yellow";
+    }
+    else{ ROUNDWINNER.style.color = "red"}
 }
 
 
@@ -60,17 +73,41 @@ function resetGame()
 {
     if((playerScore==5) || computerScore == 5)
     {
+        modal.style.display = "block";
         playerScore = 0;
         computerScore = 0;
+        span.onclick = function() {
+            modal.style.display = "none";
+          }
+          if(playerScore == 5){
+            MODAL_TEXT.textContent = "You Win!";
+            MODAL_TEXT.style.color = 'green';
+          }
+          else{
+            MODAL_TEXT.textContent = "You Lose!";
+            MODAL_TEXT.style.color = 'red';
+          }
+        updateScore(0,0);
     }
 }
+
+function updateComputerIcon(computerchoice)
+{
+    if(computerchoice== "rock"){COMPUTERSELECTION.textContent = "✊"; }
+    else if(computerchoice == "paper"){COMPUTERSELECTION.textContent ="✋"; }
+    else{COMPUTERSELECTION.textContent ="✌"; }
+}
+
+
+
 
 function executeButton(playerInput)
 {
     computerInput = computerPlay();
+    updateComputerIcon(computerInput);
     playRound(playerInput, computerInput);
     updateScore(playerScore, computerScore);
-    console.log("you chose " +playerInput+ " and computer chose " + computerInput)
+    console.log("you chose " +playerInput+ " and computer chose " + computerInput);
     resetGame();
     displayRoundWinner();
 }
@@ -82,16 +119,19 @@ function executeButton(playerInput)
 r.addEventListener('click', () => 
 {
     executeButton("rock");
+    
 });
 
 p.addEventListener('click', () => 
 {
     executeButton("paper");
+    
 });
 
 s.addEventListener('click', () => 
 {
     executeButton("scissors");
+    
 });
 
  
